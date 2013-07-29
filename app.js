7c9ba9417d0ff9846daa8a1ad08468aa74c89377
Crae Sosa <csosa246@gmail.com>
2013-07-28 21:07:47 -0400

    init

--- app.js
+++ app.js
@@ -5,14 +5,24 @@
 
 var express = require('express'),
     routes = require('./routes'),
-    api = require('./routes/api');
+    api = require('./routes/api'),
+    http = require('http');
 
 var app = module.exports = express();
 
 // Configuration
 app.configure(function() {
+	app.set('port',process.env.PORT || 3000);
+  	app.set('views', __dirname + '/views');
+  	app.engine('.html', require('ejs').renderFile);
+  	app.set('view engine', 'html');
+  	app.set('view options', {
+    	layout: false
+  	});
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.cookieParser());
+  	app.use(express.session({ secret: 'omgnodeworks' }));
     app.use(express.static(__dirname + '/public'));
     app.use(app.router);
 });
@@ -35,7 +45,11 @@ app.get('/api/name', api.name);
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
-// Start server
-app.listen(3000, function(){
-	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// // Start server
+// app.listen(3000, function(){
+// 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+http.createServer(app).listen(app.get('port'),function(){
+    console.log("Express server listening on port: " + app.get('port'));
+})
